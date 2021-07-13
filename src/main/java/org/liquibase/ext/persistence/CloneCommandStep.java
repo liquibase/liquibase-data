35285bf9c928224ef99a11673e85a1c7d20fdfb7
Derek Smart <derek@grindaga.com>
2021-07-13 15:36:53 -0400

    convert to command extension
    Signed-off-by: Derek Smart <derek@grindaga.com>

---
+++ src/main/java/org/liquibase/ext/persistence/CloneCommandStep.java
@@ -0,0 +1,16 @@
+package org.liquibase.ext.persistence;
+
+import liquibase.command.CommandResultsBuilder;
+
+public class CloneCommandStep extends liquibase.command.AbstractCommandStep {
+
+    @Override
+    public String[][] defineCommandNames() {
+        return new String[0][];
+    }
+
+    @Override
+    public void run(CommandResultsBuilder commandResultsBuilder) throws Exception {
+
+    }
+}
