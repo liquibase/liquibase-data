e05378db8dcc346db9b4b26d9c4488b991e66db4
Derek Smart <derek@grindaga.com>
2021-08-27 14:43:30 -0400

    generateChangeLog completed
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/main/java/org/liquibase/ext/persistence/GenerateChangeLogCommandStep.java
+++ src/main/java/org/liquibase/ext/persistence/GenerateChangeLogCommandStep.java
@@ -1,30 +1,75 @@
 package org.liquibase.ext.persistence;
 
-import liquibase.command.CommandArgumentDefinition;
-import liquibase.command.CommandBuilder;
-import liquibase.command.CommandDefinition;
-import liquibase.command.CommandResultsBuilder;
+import liquibase.Scope;
+import liquibase.command.*;
+import liquibase.configuration.ConfigurationValueObfuscator;
+import liquibase.ui.UIService;
 
-public class GenerateChangeLogCommandStep extends liquibase.command.AbstractCommandStep {
+import java.util.List;
 
-    public static final String[] COMMAND_NAME = new String[]{ "generateChangelog" };
-    public static final CommandArgumentDefinition<String> TARGETDB;
-    public static final CommandArgumentDefinition<String> TARGETSTAGE;
+public class GenerateChangeLogCommandStep extends TitanBase {
+
+    public static final String[] COMMAND_NAME = new String[]{ "titan", "generateChangelog" };
+    public static final CommandArgumentDefinition<String> TARGET_DB;
+    public static final CommandArgumentDefinition<String> TARGET_STATE;
+
+    //Copied from liquibase-core/src/main/java/liquibase/command/core/GenerateChangelogCommandStep.java
+    public static final CommandArgumentDefinition<String> USERNAME_ARG;
+    public static final CommandArgumentDefinition<String> PASSWORD_ARG;
+    public static final CommandArgumentDefinition<String> URL_ARG;
+    public static final CommandArgumentDefinition<String> CHANGELOG_FILE_ARG;
+    public static final CommandArgumentDefinition<String> DATA_OUTPUT_DIRECTORY;
+    public static final CommandArgumentDefinition<String> EXCLUDE_OBJECTS_ARG;
+    public static final CommandArgumentDefinition<String> INCLUDE_OBJECTS_ARG;
+    public static final CommandArgumentDefinition<String> SCHEMAS_ARG;
+    public static final CommandArgumentDefinition<String> DIFF_TYPES_ARG;
+    public static final CommandArgumentDefinition<String> DRIVER_ARG;
+    public static final CommandArgumentDefinition<String> DRIVER_PROPERTIES_FILE_ARG;
+    public static final CommandArgumentDefinition<String> OVERWRITE_OUTPUT_FILE_ARG;
 
     static {
         CommandBuilder builder = new CommandBuilder(COMMAND_NAME);
-        TARGETDB = builder.argument("targetDB", String.class)
+        TARGET_DB = builder.argument("targetDB", String.class)
                 .description("Name of Target Database")
-                //.required()
+                .required()
                 .build();
-        TARGETSTAGE = builder.argument("targetState", String.class)
+        TARGET_STATE = builder.argument("targetState", String.class)
                 .description("Commit for Target Database, defaults to current (optional)")
                 .build();
+
+        //Copied from liquibase-core/src/main/java/liquibase/command/core/GenerateChangelogCommandStep.java
+        URL_ARG = builder.argument("url", String.class).required()
+                .description("The JDBC database connection URL").build();
+        USERNAME_ARG = builder.argument("username", String.class)
+                .description("Username to use to connect to the database").build();
+        PASSWORD_ARG = builder.argument("password", String.class)
+                .description("Password to use to connect to the database")
+                .setValueObfuscator(ConfigurationValueObfuscator.STANDARD)
+                .build();
+        DRIVER_ARG = builder.argument("driver", String.class)
+                .description("The JDBC driver class").build();
+        DRIVER_PROPERTIES_FILE_ARG = builder.argument("driverPropertiesFile", String.class)
+                .description("The JDBC driver properties file").build();
+        CHANGELOG_FILE_ARG = builder.argument("changelogFile", String.class).required()
+                .description("File to write changelog to").build();
+        DATA_OUTPUT_DIRECTORY = builder.argument("dataOutputDirectory", String.class)
+                .description("Directory to write table data to").build();
+        EXCLUDE_OBJECTS_ARG = builder.argument("excludeObjects", String.class)
+                .description("Objects to exclude from diff").build();
+        INCLUDE_OBJECTS_ARG = builder.argument("includeObjects", String.class)
+                .description("Objects to include in diff").build();
+        SCHEMAS_ARG = builder.argument("schemas", String.class)
+                .description("Schemas to include in diff").build();
+        DIFF_TYPES_ARG = builder.argument("diffTypes", String.class)
+                .description("Types of objects to compare").build();
+        OVERWRITE_OUTPUT_FILE_ARG = builder.argument("overwriteOutputFile", String.class)
+                .description("Flag to allow overwriting of output changelog file").build();
     }
 
     @Override
     public void adjustCommandDefinition(CommandDefinition commandDefinition) {
-        //commandDefinition.setShortDescription("");
+        commandDefinition.setShortDescription("Generate a changelog");
+        commandDefinition.setLongDescription("Writes Change Log XML to copy the current state of the database to standard out or a file");
     }
 
     @Override
@@ -33,7 +78,24 @@ public class GenerateChangeLogCommandStep extends liquibase.command.AbstractComm
     }
 
     @Override
-    public void run(CommandResultsBuilder commandResultsBuilder) throws Exception {
+    public void run(CommandResultsBuilder resultsBuilder) throws Exception {
+        String targetDB = resultsBuilder.getCommandScope().getArgumentValue(TARGET_DB);
+        String targetState = resultsBuilder.getCommandScope().getArgumentValue(TARGET_STATE);
 
+        if (targetDB != null && targetState != null) {
+            List<String> args = BuildArgs("titan", "checkout", "-c", targetState, targetDB);
+            CE.exec(args);
+            Thread.sleep(3000);
+        }
+
+        try {
+            final CommandScope commandScope = new CommandScope("generateChangelog");
+            commandScope.setOutput(resultsBuilder.getOutputStream());
+            CommandResults result = commandScope.execute();
+        } catch (Exception e) {
+            UIService ui = Scope.getCurrentScope().getUI();
+            ui.sendErrorMessage(e.getMessage());
+            throw new Exception(e);
+        }
     }
 }
